b72ef84000618c39ab1522422a2f12fdc09fd02a
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-08-12 10:49:48 +0700

    add some component and footer in pages

---
+++ components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="text-4xl font-bold text-black mt-[6rem]">Im a hero</div>
+    </div>
+  );
+};
+
+export default Hero;
