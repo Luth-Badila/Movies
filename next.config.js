76b90ad1c612f0ae0edafa43ff981be413a1c5d6
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-08-18 12:21:33 +0700

    make change the next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = {
-  domains: ["images.unsplash.com", "www.sanity.io", "e7.pngegg.com", "w7.pngwing.com", "image.tmdb.org"],
-  nextConfig,
+  images: {
+    domains: ["images.unsplash.com", "www.sanity.io", "e7.pngegg.com", "w7.pngwing.com", "image.tmdb.org"],
+  },
   env: {
     NEXT_TMDB_KEY: process.env.NEXT_TMDB_KEY,
     NEXT_BASE_URL: process.env.NEXT_BASE_URL,
     NEXT_NEXT_IMG_URL: process.env.NEXT_IMG_URL,
   },
 };
+
+module.exports = nextConfig;
